072df2ebc04d77b32985114fa25f5303076f91b4
bigbandzjosh <joosh.scott@hotmail.com>
2024-11-15 15:26:34 -0400

    working now

--- HeatingDegreeDays.py
+++ HeatingDegreeDays.py
@@ -1,3 +1,7 @@
+import csv 
+
+
+# Temperature classs
 class Temperature:
     def __init__(self, temperature, unit='C'):
         self.temperature = temperature
@@ -15,7 +19,33 @@ class Temperature:
     
     def __str__(self):
         return f'{self.temperature} {self.unit}'
-    
-    #testing
-    
+     
+# Function to count the number of heating degree days, takes a list of temperatures and a base temperature
+def count_heating_degree_days(temperatures, base_temperature=18):
+    temperature = Temperature(temperatures, 'C')
+    heating_degree_days_count = 0
+    for temperature in temperatures:
+        if temperature < base_temperature:
+            heating_degree_days_count += 1
+            hdd = base_temperature - temperature
+            print(f'{temperature} < {base_temperature} = {hdd}')
+        else:
+            print(f'{temperature} >= {base_temperature} = Not a Heating Degree Day')
+    return heating_degree_days_count
+
+
+with open('en_climate_daily_NS_8205092_2023_P1D (1).csv', "r") as data_file:
+    temps = csv.reader(data_file)
+    header = next(temps)
+
+    temperatures = []
+    for data in temps:
+        mean_temp = data[13]  # Mean Temp (°C) is the 14th column (index 13)
+        if mean_temp:  # Check if the mean_temp is not empty
+            temp = float(mean_temp)
+            temperatures.append(temp)
+
+    hdd_count = count_heating_degree_days(temperatures)
+    print(f'Number of Heating Degree Days: {hdd_count}')
+    print(f'Number of temperatures: {len(temperatures)}')
 
