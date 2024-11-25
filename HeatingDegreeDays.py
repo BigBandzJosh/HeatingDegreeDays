565033a90bad6dab1a8964ec22af2381bbb1d4ee
Josh Scott <joosh.scott@hotmail.com>
2024-11-25 12:15:56 -0400

    Changed code to match what teacher wanted... I think this works as intended, and the code does feel better now.

--- HeatingDegreeDays.py
+++ HeatingDegreeDays.py
@@ -1,8 +1,9 @@
 import csv 
 
 
-# Temperature classs
+
 class Temperature:
+    """Class for the Temperature object. It has two methods to convert the temperature to Fahrenheit and Celsius"""
     def __init__(self, temperature, unit='C'):
         self.temperature = temperature
         self.unit = unit
@@ -19,39 +20,54 @@ class Temperature:
     
     def __str__(self):
         return f'{self.temperature} {self.unit}'
-     
-# Function to count the number of heating degree days, takes a list of temperatures and a base temperature
-def count_heating_degree_days(temperatures, base_temperature=18):
-    temperature = Temperature(temperatures, 'C')
+
+def count_heating_degree_days(temperature_objs, base_temperature=18):
+    """Count the number of heating degree days based on a list of Temperature objects."""
     heating_degree_days_count = 0
-    for temperature in temperatures:
-        if temperature < base_temperature:
+    for temp_obj in temperature_objs:
+        temp_celsius = temp_obj.convert_to_celsius() if temp_obj.unit == 'F' else temp_obj.temperature
+        if temp_celsius < base_temperature:
             heating_degree_days_count += 1
-            hdd = base_temperature - temperature
-            print(f'{temperature} < {base_temperature} = {hdd}')
+            print(f'{temp_obj} < {base_temperature}°C = Heating Degree Day')
         else:
-            print(f'{temperature} >= {base_temperature} = Not a Heating Degree Day')
+            print(f'{temp_obj} >= {base_temperature}°C = Not a Heating Degree Day')
     return heating_degree_days_count
 
 
-with open('en_climate_daily_NS_8205092_2023_P1D (1).csv', "r") as data_file:
-    temps = csv.reader(data_file)
-    header = next(temps)
 
-    temperatures = []
-    date_times = []
-    for data in temps:
-        mean_temp = data[13]  # Mean Temp (°C) is the 14th column 
-        date_time = data[4] # Trying to append the date for each temperature reading to show which day the temperature was recorded
-        if mean_temp:  # Check if the mean_temp is not empty
-            temp = float(mean_temp)
-            temperatures.append(temp)
-            date_times.append(date_time)
+# with open('en_climate_daily_NS_8205092_2023_P1D (1).csv', "r") as data_file:
+#     temps = csv.reader(data_file)
+#     header = next(temps)
 
-            
+#     temperatures = []
+#     date_times = []
+#     for data in temps:
+#         mean_temp = data[13]  # Mean Temp (°C) is the 14th column 
+#         date_time = data[4] # Trying to append the date for each temperature reading to show which day the temperature was recorded
+#         if mean_temp:  # Check if the mean_temp is not empty
+#             temp = float(mean_temp)
+#             temperatures.append(temp)
+#             date_times.append(date_time)
 
-    hdd_count = count_heating_degree_days(temperatures)
+#     hdd_count = count_heating_degree_days(temperatures)
 
-    print(f'Number of Heating Degree Days: {hdd_count}')
-    print(f'Number of temperatures reading from dataset: {len(temperatures)}')
+
+if __name__ == '__main__':
+     with open("en_climate_daily_NS_8205092_2023_P1D (1).csv", "r") as file:
+        header = file.readline()
+        # print(header)
+        
+        temperatures = []
+        date_times = []
+        for data in csv.reader(file):
+            mean_temp = data[13]
+            date_time = data[4]
+            if mean_temp:
+                temp = Temperature(float(mean_temp), 'C')
+                temperatures.append(temp)
+                date_times.append(date_time)
+        
+        hdd_count = count_heating_degree_days(temperatures)
+        print(f'Number of Heating Degree Days: {hdd_count}')
+        print(f'Total number of temperature readings: {len(temperatures)}')
 
--- hdd_test.py
+++ hdd_test.py
@@ -1,9 +1,16 @@
 import unittest
-import HeatingDegreeDays
+from HeatingDegreeDays import Temperature, count_heating_degree_days
 
 class TestHDD(unittest.TestCase):
-      def testHDD1(self):
-          self.assertEqual(HeatingDegreeDays.count_heating_degree_days([15, 18, 20, 25, 30, 35]), 1)
+    def testHDD1(self):
+        # Create Temperature objects with values in Celsius
+        temperatures = [Temperature(15), Temperature(18), Temperature(20), Temperature(25), Temperature(30), Temperature(35)]
+        self.assertEqual(count_heating_degree_days(temperatures), 1)
 
-      def testHDD2(self):
-          self.assertEqual(HeatingDegreeDays.count_heating_degree_days([15, 18, 20, 25, 3, 12]), 3)
+    def testHDD2(self):
+        # Create Temperature objects with values in Celsius
+        temperatures = [Temperature(15), Temperature(18), Temperature(20), Temperature(25), Temperature(3), Temperature(12)]
+        self.assertEqual(count_heating_degree_days(temperatures), 3)
+
+if __name__ == '__main__':
+    unittest.main()
